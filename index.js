7662a867e218953a943a944ef0767a2113f1798b
Randell Dawson <randy@randydawson.com>
2022-08-06 16:07:04 -0600

    fix: get proxy forwarded ip if behind proxy

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
 app.get('/api/whoami', function (req, res) {
-  res.json({ ipaddress: req.ip, language: "English", software: "NodeJS" });
+  const ipaddress = req.header('x-forwarded-for') || req.ip;
+  res.json({
+    ipaddress,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
 });
+
 
 const listener = app.listen(process.env.PORT, () => {
   console.log('App is listening on port ' + listener.address().port);
